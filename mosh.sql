b03aab221620c29efe492e32288cec37e864d43d
Chris Mkolongo <chrismks23@gmail.com>
2019-08-20 23:08:19 +0300

    Add files via upload

--- mosh.sql
+++ mosh.sql
@@ -29,29 +29,95 @@ ORDER BY total_price DESC;
 
 SELECT * FROM customers ORDER BY points DESC LIMIT 3;
 
+-- joins
 SELECT order_id, p.product_id, p.name, o.quantity, o.unit_price
 FROM order_items o
 JOIN products p
+	-- USING (product_id)
 	ON o.product_id = p.product_id;
 
+-- multiple joins
 SELECT p.payment_id, p.date,
 	   c.name, c.phone,
        pm.name
 FROM payments p
 JOIN clients c
+	-- USING (client_id)
 	ON p.client_id = c.client_id
 JOIN payment_methods pm
 	ON p.payment_method = pm.payment_method_id;
 
+-- outer joins
+SELECT p.product_id,
+	   p.name,
+       oi.quantity
+FROM products p
+LEFT JOIN order_items oi
+	-- USING (product_id)
+	ON p.product_id = oi.product_id;
+
+-- multiple outer joins
+SELECT o.order_date,
+	   o.order_id,
+       c.first_name,
+       sh.name AS shipper,
+       os.name
+FROM orders o
+JOIN customers c
+	-- USING (customer_id)
+	ON o.customer_id = c.customer_id
+LEFT JOIN shippers sh
+	-- USING (shipper_id)
+	ON o.shipper_id = sh.shipper_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id;
+
+-- multiple joins
+SELECT p.date,
+	   c.name AS client,
+       p.amount,
+       pm.name AS payment_method
+FROM payments p
+JOIN clients c
+	-- USING (client_id)
+	ON p.client_id = c.client_id
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id;
+
+-- cross joins
+SELECT * FROM shippers, products;
+SELECT * FROM shippers
+CROSS JOIN products;
+
+-- using UNIONS
+SELECT customer_id, first_name, points, 'Bronze' AS type
+FROM customers
+WHERE points < 2000
+UNION
+SELECT customer_id, first_name, points, 'Silver' AS type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+UNION
+SELECT customer_id, first_name, points, 'Gold' AS type
+FROM customers
+WHERE points > 3000
+ORDER BY first_name;
 
 
+-- CREATE TABLE invoices_archieved AS;
+SELECT * 
+FROM invoices i
+JOIN clients c
+-- USING (client_id)
+    ON i.client_id = c.client_id
+WHERE i.payment_date IS NOT NULL;
 
 
-
-
-
-
-
+UPDATE orders SET comments = "Gold Customer"
+WHERE customer_id IN (
+		SELECT customer_id 
+			FROM customers 
+            WHERE points > 3000);
 
 
 
