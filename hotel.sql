bf486a5e33af99d9343bf620eda49547ecd2e48a
Chris Mkolongo <chrismks23@gmail.com>
2019-08-21 20:25:22 +0300

    Update hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -156,23 +156,18 @@ SELECT name,
 FROM games;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- alternative to the above query
+SELECT 
+	name,
+	CASE
+		WHEN EXTRACT(HOUR FROM start) < 12 THEN 'Morning'
+		WHEN EXTRACT(HOUR FROM start) < 18 THEN 'Afternoon'
+		ELSE 'Evening'
+	END AS 'Part of the Day',
+	CASE
+		WHEN duration <= 3 THEN 'Extra Short'
+		WHEN duration <= 6 THEN 'Short'
+		WHEN duration <= 10 THEN 'Long'
+		ELSE 'Extra Long'
+	END AS Duration
+FROM games;
